2e5c4e2159df379531d94c8301a608e29b0dcc20
BBJohns05 <thepurpleblur@gmail.com>
2022-09-06 14:42:41 -0700

    JS and small fix on the description
    Rephrased the description of website and also worked on the JS for the conversation.

--- files/pages/numberConverter.js
+++ files/pages/numberConverter.js
@@ -1,3 +1,55 @@
 function numberConvert(){
-    console.log("placeholder command")
+    prompt("Insert your binary number.")
+    var x = answer;
+    x = parseInt(x, 2);//Convert binary to hex
+    x = x.toString(16);//Convert decimal to hex
+    alert(hexadecimal);
+}
+if(remainder1 == 0){
+    remainder1 = "0";
+}
+if(remainder1 == 1){
+    remainder1 = "1";
+}
+if(remainder1 == 2){
+    remainder1 = "2";
+}
+if(remainder1 == 3){
+    remainder1 = "3";
+}
+if(remainder1 == 4){
+    remainder1 = "4";
+}
+if(remainder1 == 5){
+    remainder1 = "5";
+}
+if(remainder1 == 6){
+    remainder1 = "6";
+}
+if(remainder1 == 7){
+    remainder1 = "7";
+}
+if(remainder1 == 8){
+    remainder1 = "8";
+}
+if(remainder1 == 9){
+    remainder1 = "9";
+}
+if(remainder1 == 10){
+    remainder1 = "A";
+}
+if(remainder1 == 11){
+    remainder1 = "B";
+}
+if(remainder1 == 12){
+    remainder1 = "C";
+}
+if(remainder1 == 13){
+    remainder1 = "D";
+}
+if(remainder1 == 14){
+    remainder1 = "E";
+}
+if(remainder1 == 15){
+    remainder1 = "F";
 }
