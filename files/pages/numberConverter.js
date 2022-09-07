703604c8b202a35dee6e101896a3c7d8c0f76fc7
MercutioG <110576904+MercutioG@users.noreply.github.com>
2022-09-06 17:00:29 -0700

    Update numberConverter.js

--- files/pages/numberConverter.js
+++ files/pages/numberConverter.js
@@ -8,22 +8,3 @@ function numberConvert(){
     y = y.toString(16); //Convert decimal to hex
     alert(y);
 }
-
- if(remainder1 == 10){
-     remainder1 = "A";
- }
- if(remainder1 == 11){
-     remainder1 = "B";
- }
- if(remainder1 == 12){
-     remainder1 = "C";
- }
- if(remainder1 == 13){
-     remainder1 = "D";
- }
- if(remainder1 == 14){
-     remainder1 = "E";
- }
- if(remainder1 == 15){
-     remainder1 = "F";
- }
