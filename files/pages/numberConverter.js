ad13fde0927da816db78398d4393285214aa8a09
BBJohns05 <thepurpleblur@gmail.com>
2022-09-06 17:00:42 -0700

    JS Working (converting)
    Now the alert (the conversion) will be one alert and there are spaces between the binary and hex so it looks nice.

--- files/pages/numberConverter.js
+++ files/pages/numberConverter.js
@@ -2,28 +2,9 @@ function numberConvert(){
     let answer = parseInt(prompt("Insert your decimal number."));
     var x = answer;
     x = x.toString(2);  //Convert decimal to binary
-    alert(x);
 
     var y = answer
     y = y.toString(16); //Convert decimal to hex
-    alert(y);
-}
 
- if(remainder1 == 10){
-     remainder1 = "A";
- }
- if(remainder1 == 11){
-     remainder1 = "B";
- }
- if(remainder1 == 12){
-     remainder1 = "C";
- }
- if(remainder1 == 13){
-     remainder1 = "D";
- }
- if(remainder1 == 14){
-     remainder1 = "E";
- }
- if(remainder1 == 15){
-     remainder1 = "F";
- }
+    alert("Decimal To Binary " + x +"    Decimal To Hexadecimal " + y)
+    }
