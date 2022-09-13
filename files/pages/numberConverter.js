43125e3718d5f3a04a1d00eaf905b2feb2d5c0bc
MercutioG <110576904+MercutioG@users.noreply.github.com>
2022-09-12 17:22:34 -0700

    Fixed Index.html
    - Moved html to the outside
    - Adjusted html to the new change
    - Fixed JS

--- files/pages/numberConverter.js
+++ files/pages/numberConverter.js
@@ -1,10 +1,14 @@
+var answer = 0
+var x = 0
+var y = 0
+
 function numberConvert(){
     let answer = parseInt(prompt("Insert your decimal number."));
     var x = answer;
     x = x.toString(2);  //Convert decimal to binary
-    alert(x);
 
     var y = answer
     y = y.toString(16); //Convert decimal to hex
-    alert(y);
+
+    alert("Your number is " + x + " in binary, and " + y + " in hexadecimal.");
 }
